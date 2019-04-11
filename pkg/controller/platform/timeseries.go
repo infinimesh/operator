522dad3800c7b2f23200da22ceedea2bffd18e08
Johannes Brüderl <johannes.bruederl@gmail.com>
2019-04-11 02:21:15 +0200

    add timescale reconciler

---
+++ pkg/controller/platform/timeseries.go
@@ -0,0 +1,95 @@
+package platform
+
+import (
+	"context"
+	"reflect"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	infinimeshv1beta1 "github.com/infinimesh/operator/pkg/apis/infinimesh/v1beta1"
+)
+
+func (r *ReconcilePlatform) reconcileTimeseries(request reconcile.Request, instance *infinimeshv1beta1.Platform) error {
+	log := logger.WithName("time series")
+	{
+		deploymentName := instance.Name + "-timescale-connector"
+		// TODO(user): Change this to be the object type created by your controller
+		// Define the desired Deployment object
+
+		deploy := &appsv1.Deployment{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      deploymentName,
+				Namespace: instance.Namespace,
+			},
+			Spec: appsv1.DeploymentSpec{
+				Selector: &metav1.LabelSelector{
+					MatchLabels: map[string]string{"deployment": deploymentName},
+				},
+				Template: corev1.PodTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"deployment": deploymentName}},
+					Spec: corev1.PodSpec{
+						Containers: []corev1.Container{
+							{
+								Name:            "timescale-connector",
+								Image:           "quay.io/infinimesh/timescale-connector:latest",
+								ImagePullPolicy: corev1.PullAlways,
+								EnvFrom: []corev1.EnvFromSource{
+									{
+										SecretRef: &corev1.SecretEnvSource{
+											LocalObjectReference: corev1.LocalObjectReference{
+												Name: instance.Name + "-timescaledb-auth",
+											},
+										},
+									},
+								},
+								Env: []corev1.EnvVar{
+									{
+										Name:  "KAFKA_HOST",
+										Value: instance.Spec.Kafka.BootstrapServers,
+									},
+									{
+										Name:  "DB_ADDR",
+										Value: "postgres://$(POSTGRES_USER):$(POSTGRES_PASSWORD)@" + instance.Name + "-timescaledb/postgres?sslmode=disable",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+
+		if err := controllerutil.SetControllerReference(instance, deploy, r.scheme); err != nil {
+			return err
+		}
+
+		found := &appsv1.Deployment{}
+		err := r.Get(context.TODO(), types.NamespacedName{Name: deploy.Name, Namespace: deploy.Namespace}, found)
+		if err != nil && errors.IsNotFound(err) {
+			log.Info("Creating Deployment", "namespace", deploy.Namespace, "name", deploy.Name)
+			err = r.Create(context.TODO(), deploy)
+			if err != nil {
+				return err
+			}
+		} else if err != nil {
+			return err
+		} else {
+			if !reflect.DeepEqual(deploy.Spec, found.Spec) {
+				found.Spec = deploy.Spec
+				log.Info("Updating Deployment", "namespace", deploy.Namespace, "name", deploy.Name)
+				err = r.Update(context.TODO(), found)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	return nil
+}
