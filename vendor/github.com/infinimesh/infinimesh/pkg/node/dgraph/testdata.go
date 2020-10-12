4279bc9042379c728ab5eed64562e25d9bb8c312
Ankit Trivedi <ankit_trivedi_15@yahoo.com>
2020-10-12 15:36:27 +0200

    updated the test data file

--- vendor/github.com/infinimesh/infinimesh/pkg/node/dgraph/testdata.go
+++ vendor/github.com/infinimesh/infinimesh/pkg/node/dgraph/testdata.go
@@ -1,3 +1,20 @@
+//--------------------------------------------------------------------------
+// Copyright 2018 Infinite Devices GmbH
+// www.infinimesh.io
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//--------------------------------------------------------------------------
+
 package dgraph
 
 import (
@@ -11,6 +28,7 @@ import (
 	"github.com/infinimesh/infinimesh/pkg/node/nodepb"
 )
 
+//ImportSchema is a method to import the schema in Dgraph DB for tests
 func ImportSchema(dg *dgo.Dgraph, drop bool) error {
 	if drop {
 		err := dg.Alter(context.Background(), &api.Operation{DropAll: drop})
@@ -20,8 +38,14 @@ func ImportSchema(dg *dgo.Dgraph, drop bool) error {
 	}
 	schema := `
   tags: [string] .
+  fingerprint.algorithm: string .
   name: string @index(exact) .
   username: string @index(exact) .
+  enabled: bool @index(bool) .
+  isRoot: bool @index(bool) .
+  isAdmin: bool @index(bool) .
+  markfordeletion: bool @index(bool) .
+  deleteinitiationtime: datetime @index(day) .
   action: string @index(term) .
   type: string @index(exact) .
   access.to: uid @reverse .
@@ -40,23 +64,27 @@ func ImportSchema(dg *dgo.Dgraph, drop bool) error {
 
 }
 
+//ImportStandardSet is a method to impor the test data for tests
 func ImportStandardSet(repo node.Repo) (userID string, adminID string, err error) {
 	// careful,  currently when referencing a namespace, the name of it has to be used, not the id (0x...)
 	sharedNs := "shared-project"
-	_, err = repo.CreateNamespace(context.Background(), sharedNs)
+	namespace, err := repo.CreateNamespace(context.Background(), sharedNs)
 	if err != nil {
+		fmt.Println("Create Namespace failed", err)
 		return "", "", err
 	}
 
 	ns := "joe"
-	joe, err := repo.CreateUserAccount(context.Background(), "joe", "test123", false, true)
+	joe, err := repo.CreateUserAccount(context.Background(), "joe", "test123", false, false, true)
 	if err != nil {
+		fmt.Println("Create Account failed for joe", err)
 		return "", "", err
 	}
 	fmt.Println("User joe: ", joe)
 
-	hanswurst, err := repo.CreateUserAccount(context.Background(), "hanswurst", "hanswurst", false, true)
+	hanswurst, err := repo.CreateUserAccount(context.Background(), "hanswurst", "hanswurst", false, false, true)
 	if err != nil {
+		fmt.Println("Create Account failed for hans", err)
 		return "", "", err
 	}
 
@@ -64,85 +92,101 @@ func ImportStandardSet(repo node.Repo) (userID string, adminID string, err error
 
 	// Authorize both users on a shared project
 	{
-		err = repo.AuthorizeNamespace(context.Background(), joe, sharedNs, nodepb.Action_WRITE)
+		err = repo.AuthorizeNamespace(context.Background(), joe, namespace, nodepb.Action_WRITE)
 		if err != nil {
+			fmt.Println("Authorize Namespace failed for joe", err)
 			return "", "", err
 		}
 
-		err = repo.AuthorizeNamespace(context.Background(), hanswurst, sharedNs, nodepb.Action_WRITE)
+		err = repo.AuthorizeNamespace(context.Background(), hanswurst, namespace, nodepb.Action_WRITE)
 		if err != nil {
+			fmt.Println("Authorize Namespace failed for hans", err)
 			return "", "", err
 		}
 	}
 
-	admin, err := repo.CreateUserAccount(context.Background(), "admin", "admin123", true, true)
+	admin, err := repo.CreateUserAccount(context.Background(), "admin", "admin123", true, true, true)
 	if err != nil {
+		fmt.Println("Create Account failed", err)
 		return "", "", err
 	}
 	fmt.Println("Admin: ", admin)
 
 	building, err := repo.CreateObject(context.Background(), "Angerstr 14", "", node.KindAsset, ns)
 	if err != nil {
+		fmt.Println("Create Object failed", err)
 		return "", "", err
 	}
 
 	first, err := repo.CreateObject(context.Background(), "First Floor", building, node.KindAsset, ns)
 	if err != nil {
+		fmt.Println("Create Object failed", err)
 		return "", "", err
 	}
 
 	_, err = repo.CreateObject(context.Background(), "Second Floor", building, node.KindAsset, ns)
 	if err != nil {
+		fmt.Println("Create Object failed", err)
 		return "", "", err
 	}
 
 	apartment1Right, err := repo.CreateObject(context.Background(), "Apartment right side", first, node.KindAsset, ns)
 	if err != nil {
+		fmt.Println("Create Object failed", err)
 		return "", "", err
 	}
 
 	_, err = repo.CreateObject(context.Background(), "Entrance", apartment1Right, node.KindAsset, ns)
 	if err != nil {
+		fmt.Println("Create Object failed", err)
 		return "", "", err
 	}
 
 	_, err = repo.CreateObject(context.Background(), "Bathroom", apartment1Right, node.KindAsset, ns)
 	if err != nil {
+		fmt.Println("Create Object failed", err)
 		return "", "", err
 	}
 
 	_, err = repo.CreateObject(context.Background(), "Kitchen", apartment1Right, node.KindAsset, ns)
 	if err != nil {
+		fmt.Println("Create Object failed", err)
 		return "", "", err
 	}
 
 	_, err = repo.CreateObject(context.Background(), "Bedroom", apartment1Right, node.KindAsset, ns)
 	if err != nil {
+		fmt.Println("Create Object failed", err)
 		return "", "", err
 	}
 
 	_, err = repo.CreateObject(context.Background(), "Kinderzimmer", apartment1Right, node.KindAsset, ns)
 	if err != nil {
+		fmt.Println("Create Object failed", err)
 		return "", "", err
 	}
 
 	_, err = repo.CreateObject(context.Background(), "Walk-through room", apartment1Right, node.KindAsset, ns)
 	if err != nil {
+		fmt.Println("Create Object failed", err)
 		return "", "", err
 	}
 
 	livingRoom, err := repo.CreateObject(context.Background(), "Living room", apartment1Right, node.KindAsset, ns)
 	if err != nil {
+		fmt.Println("Create Object failed", err)
 		return "", "", err
 	}
 
 	_, err = repo.CreateObject(context.Background(), "Test-device", livingRoom, node.KindDevice, ns)
 	if err != nil {
+		fmt.Println("Create Object failed", err)
 		return "", "", err
 	}
 
 	_, err = repo.CreateObject(context.Background(), "Test-device-no-parent", "", node.KindDevice, ns)
 	if err != nil {
+		fmt.Println("Create Object failed", err)
 		return "", "", err
 	}
 
