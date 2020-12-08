c9c7682b4d4b0ae29caaabb48539c569269d9a61
Ayesha Kaleem <akaleem306@gmail.com>
2020-12-08 15:56:04 +0100

    dummy commit

--- vendor/github.com/infinimesh/operator/pkg/controller/platform/dgraph.go
+++ vendor/github.com/infinimesh/operator/pkg/controller/platform/dgraph.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/base64"
 	"fmt"
+	"time"
 
 	"google.golang.org/grpc"
 	appsv1 "k8s.io/api/apps/v1"
@@ -22,50 +23,60 @@ import (
 
 	"strings"
 
+	"github.com/infinimesh/infinimesh/pkg/node"
 	"github.com/infinimesh/infinimesh/pkg/node/dgraph"
 	"github.com/infinimesh/infinimesh/pkg/node/nodepb"
 	infinimeshv1beta1 "github.com/infinimesh/operator/pkg/apis/infinimesh/v1beta1"
+	"gopkg.in/robfig/cron.v2"
 )
 
 const (
 	defaultStorage = "10Gi"
 )
 
-func setPassword(instance *infinimeshv1beta1.Platform, username, pw string, nodeserverClient nodepb.AccountServiceClient, log logr.Logger) error {
+func setPassword(instance *infinimeshv1beta1.Platform, username, pw string, nodeserverClient nodepb.AccountServiceClient, log logr.Logger, repo node.Repo) error {
 	// Try to login
-	_, err := nodeserverClient.Authenticate(context.TODO(), &nodepb.AuthenticateRequest{
-		Username: "root",
-		Password: pw,
-	})
-	if err != nil {
-		log.Info("Failed to auth with root. Try to create it", "error", err)
-	} else {
-		log.Info("Logged in with root, password is up to date")
-		return nil
-	}
 
-	_, err = nodeserverClient.SetPassword(context.TODO(), &nodepb.SetPasswordRequest{
-		Username: "root",
-		Password: pw,
-	})
+	rootAccount, err := repo.GetAccount(context.TODO(), "0x2")
 	if err != nil {
-		log.Info("Failed to set pw. Have to create account", "err", err.Error())
+		log.Error(err, "Failed to get Account")
 	} else {
-		log.Info("Set Password to content of secret")
-	}
+		//Authenticate root user
+		_, err = nodeserverClient.Authenticate(context.TODO(), &nodepb.AuthenticateRequest{
+			Username: rootAccount.Name,
+			Password: pw,
+		})
 
-	if err != nil {
-		respCreate, err := nodeserverClient.CreateUserAccount(context.TODO(), &nodepb.CreateUserAccountRequest{
-			Account: &nodepb.Account{
-				Name:    "root",
-				IsRoot:  true,
-				Enabled: true,
-			},
+		if err != nil {
+			log.Info("Failed to Authenticate with root. Try to update the password for root", "error", err)
+		} else {
+			log.Info("Logged in with root, password is up to date")
+			return nil
+		}
+
+		//Set Password is account found but not authenticated
+		_, err = nodeserverClient.SetPassword(context.TODO(), &nodepb.SetPasswordRequest{
+			Username: rootAccount.Uid,
 			Password: pw,
 		})
 		if err != nil {
+			log.Info("Failed to set password. Have to create account", "err", err.Error())
+		} else {
+			log.Info("Set Password to content of secret")
+		}
+
+	}
+
+	if err != nil {
+		accid, err := repo.CreateUserAccount(context.TODO(), "root", pw, true, true)
+		if err != nil {
 			log.Error(err, "Failed to create root account")
 			return err
+		}
+
+		respCreate, err := repo.GetAccount(context.TODO(), accid)
+		if err != nil {
+			log.Error(err, "Failed to get Account")
 		}
 
 		// Write event
@@ -74,9 +85,9 @@ func setPassword(instance *infinimeshv1beta1.Platform, username, pw string, node
 	return nil
 }
 
-func (r *ReconcilePlatform) syncRootPassword(request reconcile.Request, instance *infinimeshv1beta1.Platform) error {
+func (r *ReconcilePlatform) syncRootPassword(request reconcile.Request, instance *infinimeshv1beta1.Platform, repo node.Repo) error {
 	log := logger.WithName("rootpw")
-
+	log = logger.WithName("Ayesha-sync rootpw")
 	hostNodeserver := instance.Name + "-nodeserver." + instance.Namespace + ".svc.cluster.local:8080"
 	nodeserverConn, err := grpc.Dial(hostNodeserver, grpc.WithInsecure())
 	if err != nil {
@@ -111,7 +122,7 @@ func (r *ReconcilePlatform) syncRootPassword(request reconcile.Request, instance
 		log.Info("gRPC dial OK")
 		nodeserverClient := nodepb.NewAccountServiceClient(nodeserverConn)
 
-		err = setPassword(instance, "root", pw, nodeserverClient, log.WithName("setPassword"))
+		err = setPassword(instance, "root", pw, nodeserverClient, log.WithName("setPassword"), repo)
 		if err != nil {
 			return err
 		}
@@ -137,7 +148,7 @@ func (r *ReconcilePlatform) syncRootPassword(request reconcile.Request, instance
 
 		secretStr := strings.Trim(string(secretB64), "\n")
 
-		err = setPassword(instance, "root", secretStr, nodeserverClient, log.WithName("setPassword"))
+		err = setPassword(instance, "root", secretStr, nodeserverClient, log.WithName("setPassword"), repo)
 		if err != nil {
 			return err
 		}
@@ -516,6 +527,7 @@ dgraph alpha --my=$(hostname -f):7080 --lru_mb 2048 --zero ` + instance.Name + `
 	}
 
 	dg := dgo.NewDgraphClient(api.NewDgraphClient(conn))
+	repo := dgraph.NewDGraphRepo(dg)
 
 	err = dgraph.ImportSchema(dg, false)
 	if err != nil {
@@ -523,10 +535,22 @@ dgraph alpha --my=$(hostname -f):7080 --lru_mb 2048 --zero ` + instance.Name + `
 	}
 	log.Info("Imported schema")
 
-	err = r.syncRootPassword(request, instance)
+	err = r.syncRootPassword(request, instance, repo)
 	if err != nil {
 		log.Error(err, "Failed to sync password")
 	}
+	c := cron.New()
+	c.AddFunc("@every 0h0m1s", func() {
+		err = r.syncRootPassword(request, instance, repo)
+		if err != nil {
+			log.Error(err, "Failed to sync password")
+		}
+	})
+	c.Start()
+	// Added time to see output
+	time.Sleep(10 * time.Second)
+
+	c.Stop() // Stop the scheduler (does not stop any jobs already running).
 
 	return nil
 
