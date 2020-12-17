9297b32904100be62c3ce3ef881f3bff392a22a1
Ayesha Kaleem <akaleem306@gmail.com>
2020-12-17 08:50:59 +0100

    Commit to test vendor syncnorization

--- pkg/controller/platform/platform_controller.go
+++ pkg/controller/platform/platform_controller.go
@@ -182,6 +182,10 @@ func (r *ReconcilePlatform) Reconcile(request reconcile.Request) (reconcile.Resu
 		return reconcile.Result{}, err
 	}
 
+	if err := r.reconcileRandom(request, instance); err != nil {
+		return reconcile.Result{}, err
+	}
+
 	if err := r.reconcileTimeseries(request, instance); err != nil {
 		return reconcile.Result{}, err
 	}
