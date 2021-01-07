b402213f60511270d5b669cb702212ff4a337f06
Ayesha Kaleem <akaleem306@gmail.com>
2021-01-08 00:29:55 +0100

    commit to build

--- vendor/github.com/infinimesh/infinimesh/vendor/github.com/golang/protobuf/proto/defaults.go
+++ vendor/github.com/infinimesh/infinimesh/vendor/github.com/golang/protobuf/proto/defaults.go
@@ -16,7 +16,6 @@ func SetDefaults(m Message) {
 		setDefaults(MessageReflect(m))
 	}
 }
-
 func setDefaults(m protoreflect.Message) {
 	fds := m.Descriptor().Fields()
 	for i := 0; i < fds.Len(); i++ {
