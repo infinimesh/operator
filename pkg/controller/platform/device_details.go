8ca105c9556e540d513720104a2f0df5a6075734
Ayesha Kaleem <akaleem306@gmail.com>
2021-02-02 17:37:37 +0100

    commit to build

--- pkg/controller/platform/device_details.go
+++ pkg/controller/platform/device_details.go
@@ -63,6 +63,7 @@ func (r *ReconcilePlatform) reconcileDeviceDetails(request reconcile.Request, in
 		return err
 	}
 	storageClassName := "infinimesh-standard"
+
 	var pvcSpec corev1.PersistentVolumeClaimSpec
 
 	pvcSpec = corev1.PersistentVolumeClaimSpec{
